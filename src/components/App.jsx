91cf4153dc62e111705cd16eea9aa9672dbef262
Sean Huang <hyprul@ucla.edu>
2019-12-03 14:34:04 -0600

    conditional

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
+var isLoggedIn = true;
+
+function renderConditional() {
+  if (isLoggedIn) {
+    return <h1>Hello</h1>
+  }
+  else {
+    return (
       <form className="form">
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
         <button type="submit">Login</button>
       </form>
+    )
+  }
+}
+
+function App() {
+  return (
+    <div className="container">
+      {renderConditional()}
     </div>
   );
 }
