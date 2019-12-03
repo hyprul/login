75bad9b3e356a02430565d0f102b03c64d6ae2ab
Sean Huang <hyprul@ucla.edu>
2019-12-03 14:49:01 -0600

    hours

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,15 @@ import Login from './Login'
 
 var isLoggedIn = false;
 
-
+const currentTime = newDate().getHours();
 
 function App() {
   return (
     <div className="container">
-      {isLoggedIn ? <h1>Hello</h1> : <Login />}
+      {/* {isLoggedIn ? <h1>Hello</h1> : <Login />} */}
+
+      {currentTime > 12 ? <h1>Why are you still working?</h1> : null}
+
     </div>
   );
 }
