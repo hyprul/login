71c130ca6ddb95b3867362d8bf8d7d18040b8622
Sean Huang <hyprul@ucla.edu>
2019-12-03 14:52:06 -0600

    && vs ternary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div className="container">
       {/* {isLoggedIn ? <h1>Hello</h1> : <Login />} */}
 
-      {currentTime > 12 ? <h1>Why are you still working?</h1> : null}
+      {currentTime > 12 && <h1>Why are you still working?</h1>}
 
     </div>
   );
